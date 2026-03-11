cdb9ee01a8be788ca4dfcb63e1b75c055248821d
nirajmali308-collab <nirajmali308@gmail.com>
2026-03-11 14:55:49 -0400

    update rf model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -41,7 +41,7 @@ def train_fraud_model():
     model = RandomForestClassifier(
         n_estimators=100,
         max_depth=10,
-        class_weight={0:1 , 1:50},
+        class_weight='balanced',
         random_state=42
     )
 
